e9a9958c2ddaf698c7412186c0e0988e07bbfcff
workwithAarohan <aarohan.nakarmi.29@gmail.com>
2020-09-05 20:37:55 +0545

    Payment index file is added.

---
+++ resources/views/payment/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Payment Details</title>
+</head>
+<body>
+    <h3>This is where you pay your bills.</h3>
+</body>
+</html>
